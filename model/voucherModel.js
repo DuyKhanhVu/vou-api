aab8e9da63671f30743a4ded3a83b74336f2f1c8
Mr Khanh Vu Duy Khanh <vdkhanh@fossil.com>
2020-05-13 17:50:22 +0700

    Add partner_id when get one voucher

--- model/voucherModel.js
+++ model/voucherModel.js
@@ -22,7 +22,7 @@ function GetAllByIdPartner(partner_id) {
 function GetOneByPartnerId(partner_id) {
     if (partner_id) {
         var defer = q.defer();
-        var query = conn.query(`SELECT voucher.*, campaign.image as campaign_image FROM voucher, campaign 
+        var query = conn.query(`SELECT voucher.*, campaign.image as campaign_image, campaign.partner_id FROM voucher, campaign 
                                 WHERE voucher.campaign_id = campaign.id AND campaign.partner_id = ${partner_id} AND available = 1 LIMIT 1`,
             function (err, result) {
                 if (err) {
